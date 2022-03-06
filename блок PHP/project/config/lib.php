965a9f47766ae327ba091c0cfab6e975602cf6cd
MOCTNK <71684726+MOCTNK@users.noreply.github.com>
2022-03-06 18:45:34 +0700

    u
    u

--- блок PHP/project/config/lib.php
+++ блок PHP/project/config/lib.php
@@ -1,11 +1,7 @@
 <?php
 
 	function checkInt($get_int) {
-		if($get_int < 0) {
-			$minus = -1;
-		} else {
-			$minus = 1;
-		}
-		return  abs((int)$get_int);
+		$minus = ($get_int < 0) ? -1 : 1;
+		return  $minus * abs((int)$int);
 	}
 ?>
